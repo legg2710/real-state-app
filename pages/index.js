45d88aa12ef85a5dc7175ab8c962146681e98b5c
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-11-28 01:33:26 -0300

    index.js update, fretchApi.js created
    fetching api

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={"500"} height={"300"} alt="banner" />
@@ -15,7 +17,9 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
     </Box>
   </Flex>
 )
-export default function Home() {
+export default function Home({ propertyForSale, propertyForRent }) {
+  console.log(propertyForSale, propertyForRent);
+
   return (
     <Box>
       <Banner 
@@ -27,7 +31,6 @@ export default function Home() {
         buttonText="Explore Renting"
         linkName="/search?purpose=for-rent"
         imageUrl='https://images.pexels.com/photos/87223/pexels-photo-87223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-
       />
       <Flex flexwrap="wrap">
         {/*fetch*/}
@@ -42,6 +45,19 @@ export default function Home() {
         linkName="/search?purpose=for-sale"
         imageUrl='https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
       />
+        {/*fetch*/}
     </Box>
   )
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`)
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`)
+
+return {
+  props: {
+    propertyForSale: propertyForSale?.hits,
+    propertyForRent: propertyForRent?.hits,
+  }
+}
 }
